91ada1fe38ae6b4aa3216a6978b0490c004db4e6
Martin Kuhn <martin.kuhn@gmx.at>
2024-05-27 07:24:38 +0200

    div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {NgSelectModule} from "@ng-select/ng-select";
+import {FormsModule} from "@angular/forms";
+import {JsonPipe} from "@angular/common";
+
+
+interface IOption<T> {
+  label: string;
+  value: T;
+}
+
+interface ICustomer {
+  id: number;
+  name: string;
+}
+
+const customer1: ICustomer = { id: 1, name: 'Customer 1' };
+const customer2: ICustomer = { id: 2, name: 'Customer 2' };
+const options: IOption<ICustomer>[] = [
+  { label: customer1.name, value: customer1 },
+  { label: customer2.name, value: customer2 },
+];
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgSelectModule, FormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng-select-test';
+  options = options;
+  selectedCustomer = customer2;
+  selectedCustomerId = customer2.id;
 }
